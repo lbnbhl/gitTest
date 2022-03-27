0f4d5fba4942ce0b187894b60d1ab2972ccb004b
wwl <1256931384@qq.com>
2022-03-27 12:18:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wwlstark
+ * @creat 2022-03-27-12:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
