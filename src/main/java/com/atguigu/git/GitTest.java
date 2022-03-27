b97affdfdeac2dbe650ddec3f5f02a1e46034e80
wwl <1256931384@qq.com>
2022-03-27 12:57:22 +0800

    push1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hotfix git4");
         System.out.println(" git4");
+        System.out.println("push1");
     }
 }
