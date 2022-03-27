550f5c89661b0e1e3509a0546f1ec0097321dd6f
wwl <1256931384@qq.com>
2022-03-27 12:28:45 +0800

    hotfix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("hello git two");
+        System.out.println("hello git3");
     }
 }
